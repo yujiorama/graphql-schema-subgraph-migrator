b23359cb012cf9b3bc0583e92119b72836bbb746
Yuji Okazawa <yujiorama+github@gmail.com>
2025-03-16 14:32:59 +0900

    Create validator.go

---
+++ internal/validator/validator.go
@@ -0,0 +1,25 @@
+package validator
+
+type ValidationError struct {
+    Code     string
+    Message  string
+    Severity string
+    Path     []string
+}
+
+type ValidationResult struct {
+    Errors []ValidationError
+}
+
+func (r *ValidationResult) HasErrors() bool {
+    for _, err := range r.Errors {
+        if err.Severity == "error" {
+            return true
+        }
+    }
+    return false
+}
+
+func (r *ValidationResult) AddError(err ValidationError) {
+    r.Errors = append(r.Errors, err)
+}
