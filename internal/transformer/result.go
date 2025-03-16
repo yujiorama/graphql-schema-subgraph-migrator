2402861295ffce42093dc8f4802258870bee6f90
Yuji Okazawa <yujiorama+github@gmail.com>
2025-03-16 19:19:38 +0900

    Update result.go

--- internal/transformer/result.go
+++ internal/transformer/result.go
@@ -1,10 +1,12 @@
 package transformer
 
 import (
+    "bytes"
     "fmt"
     "os"
 
     "github.com/vektah/gqlparser/v2/ast"
+    "github.com/vektah/gqlparser/v2/formatter"
 )
 
 // Result は変換結果を表す構造体
@@ -17,7 +19,17 @@ func (r *Result) String() string {
     if r.schema == nil {
         return ""
     }
-    return r.schema.ToSchemaString()
+
+    var buf bytes.Buffer
+    f := formatter.NewFormatter(
+        &buf,
+        []formatter.FormatOption{
+            formatter.WithComments(),
+            formatter.WithBuiltin(),
+        }
+    )
+    f.FormatSchemaDocument(r.schema)
+    return buf.String()
 }
 
 // Save は変換後のスキーマを指定されたパスに保存する
@@ -25,5 +37,15 @@ func (r *Result) Save(path string) error {
     if r.schema == nil {
         return fmt.Errorf("schema is nil")
     }
-    return os.WriteFile(path, []byte(r.String()), 0644)
+    
+    var buf bytes.Buffer
+    f := formatter.NewFormatter(
+        &buf,
+        []formatter.FormatOption{
+            formatter.WithComments(),
+            formatter.WithBuiltin(),
+        }
+    )
+    f.FormatSchemaDocument(r.schema)
+    return os.WriteFile(path, buf.Bytes(), 0644)
 }
